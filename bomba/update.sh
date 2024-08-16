bccef848553e647519bc18a6a69af26f77a22177
Pedro Cuadra <pcuadrac@hpe.com>
2024-08-16 14:51:16 -0600

    Add service support

--- bomba/update.sh
+++ bomba/update.sh
@@ -1,3 +1,20 @@
-echo "Actualizando sistema de la bomba"
+#!/bin/bash
 
-pip3 install -r requirements.txt
+SCRIPT_PATH="${PWD}/control.py"
+
+# Copy the .in file to a staging control-bomba.service
+cp control-bomba.service.in control-bomba.service
+
+# Replace the placeholders with actual values using sed on the staging file
+sed -i "s/@USER@/$SUDO_USER/g" control-bomba.service
+sed -i "s/@SCRIPT_PATH@/$SCRIPT_PATH/g" control-bomba.service
+
+# Copy the modified staging file to the systemd directory
+sudo cp control-bomba.service /etc/systemd/system/
+
+# Reload systemd and enable the service
+sudo systemctl daemon-reload
+sudo systemctl enable control-bomba.service
+
+# Start the service
+sudo systemctl start control-bomba.service
