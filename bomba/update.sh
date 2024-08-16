852f97e2d01e9a12fb67e74ef9e389130b1500b5
Pedro Cuadra <pcuadrac@hpe.com>
2024-08-16 14:57:10 -0600

    Fix sed

--- bomba/update.sh
+++ bomba/update.sh
@@ -5,9 +5,9 @@ SCRIPT_PATH="${PWD}/control.py"
 # Copy the .in file to a staging control-bomba.service
 cp control-bomba.service.in control-bomba.service
 
-# Replace the placeholders with actual values using sed on the staging file
-sed -i "s/@USER@/$SUDO_USER/g" control-bomba.service
-sed -i "s/@SCRIPT_PATH@/$SCRIPT_PATH/g" control-bomba.service
+# Replace the placeholders with actual values using sed on the staging file (using | as delimiter)
+sed -i "s|@USER@|$SUDO_USER|g" control-bomba.service
+sed -i "s|@SCRIPT_PATH@|$SCRIPT_PATH|g" control-bomba.service
 
 # Copy the modified staging file to the systemd directory
 sudo cp control-bomba.service /etc/systemd/system/
