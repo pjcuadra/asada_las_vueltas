78039b7b4daea3b5db5c76c110cff5f7b313dad6
Pedro Cuadra <pcuadrac@hpe.com>
2024-08-18 10:25:36 -0600

    Refactor MQTT code

---
+++ bomba/bomb_control/mqtt.py
@@ -0,0 +1,83 @@
+import time
+import paho.mqtt.client as mqtt
+from datetime import datetime
+import threading
+
+sleeps_per_state = {
+    "init": 1,
+    "start": 1,
+    "started": 1,
+    "stop": 5,
+    "stopping": 5,
+    "stopped": 600
+}
+
+actuator_topic = "actuators/bomba"
+
+
+# MQTT Callback Functions
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print("Connected to MQTT Broker with SSL")
+        client.subscribe(actuator_topic)
+    else:
+        print(f"Failed to connect, return code {rc}")
+
+
+def on_message(client, userdata, msg):
+    payload = msg.payload.decode()
+    if payload == "ON":
+        userdata['sm'].events['start'] = True
+        print(f"{datetime.now()} Digital signal turned ON")
+    elif payload == "OFF":
+        userdata['sm'].events['stop'] = True
+        print(f"{datetime.now()} Digital signal turned OFF")
+
+
+class MQTTController():
+    client = None
+
+    def __init__(self, addr, port, username, password, ca_cert, sm):
+        self.sm = sm
+        self.client = mqtt.Client(userdata={"sm": sm})
+        self.client.on_connect = on_connect
+        self.client.on_message = on_message
+
+        # Set username and password for authentication
+        self.client.username_pw_set(username, password)
+
+        # Configure SSL/TLS context (only CA certificate)
+        self.client.tls_set(ca_certs=ca_cert)
+
+        # Connect to MQTT Broker
+        self.client.connect(addr, port)
+
+        # Start the MQTT client's network loop in a separate thread
+        self.client.loop_start()
+
+    def publish_data(self):
+        global sleeps_per_state
+        while True:
+            pressure = self.sm.state['pressure']
+            payload = f'{{"value": {float(pressure)}, "timestamp": {int(time.time())}, "sensor_id": "bomb-water-pressure"}}'
+            self.client.publish("sensors/bomb/water_pressure", payload)
+
+            cph1 = self.sm.state['current_ph1']
+            payload = f'{{"value": {float(cph1)}, "timestamp": {int(time.time())}, "sensor_id": "bomb-current-ph1"}}'
+            self.client.publish("sensors/bomb/current_ph1", payload)
+
+            cph2 = self.sm.state['current_ph2']
+            payload = f'{{"value": {float(cph2)}, "timestamp": {int(time.time())}, "sensor_id": "bomb-current-ph2"}}'
+            self.client.publish("sensors/bomb/current_ph1", payload)
+
+            print(f"{datetime.now()} Publishing: {pressure} psi, {cph1} A, {cph2} B")
+
+            for i in range(sleeps_per_state[self.sm.state['sm_state']]):
+                if i > sleeps_per_state[self.sm.state['sm_state']]:
+                    break
+                time.sleep(1)
+
+    def start_publisher(self):
+        self.publisher = threading.Thread(target=self.publish_data,
+                                          args=(sm.state))
+        self.publisher.start()
--- bomba/control.py
+++ bomba/control.py
@@ -1,23 +1,13 @@
 import time
-import paho.mqtt.client as mqtt
 import os
-from datetime import datetime
-import threading
 from pprint import pprint
 import keyring
 from getpass import getpass
 
 from bomb_control.sm import ControlSM
 from bomb_control.bombs import BombaModelo
+from bomb_control.mqtt import MQTTController
 
-sleeps_per_state = {
-    "init": 1,
-    "start": 1,
-    "started": 1,
-    "stop": 5,
-    "stopping": 5,
-    "stopped": 600
-}
 
 # MQTT Configuration from Environment Variables
 broker_address = os.environ.get('MQTT_BROKER_ADDRESS')
@@ -44,68 +34,13 @@ bomba = BombaModelo()
 sm = ControlSM(bomba)
 sm.start()
 
-
-# MQTT Callback Functions
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        print("Connected to MQTT Broker with SSL")
-        client.subscribe(actuator_topic)
-    else:
-        print(f"Failed to connect, return code {rc}")
-
-
-def on_message(client, userdata, msg):
-    global sm
-    payload = msg.payload.decode()
-    if payload == "ON":
-        sm.events['start'] = True
-        print(f"{datetime.now()} Digital signal turned ON")
-    elif payload == "OFF":
-        sm.events['stop'] = True
-        print(f"{datetime.now()} Digital signal turned OFF")
-
-
-def publish_data(state, sleeps_per_state, client):
-    while True:
-        pressure = state['pressure']
-        payload = f'{{"value": {float(pressure)}, "timestamp": {int(time.time())}, "sensor_id": "bomb-water-pressure"}}'
-        client.publish("sensors/bomb/water_pressure", payload)
-
-        cph1 = state['current_ph1']
-        payload = f'{{"value": {float(cph1)}, "timestamp": {int(time.time())}, "sensor_id": "bomb-current-ph1"}}'
-        client.publish("sensors/bomb/current_ph1", payload)
-
-        cph2 = state['current_ph2']
-        payload = f'{{"value": {float(cph2)}, "timestamp": {int(time.time())}, "sensor_id": "bomb-current-ph2"}}'
-        client.publish("sensors/bomb/current_ph1", payload)
-
-        print(f"{datetime.now()} Publishing: {pressure} psi, {cph1} A, {cph2} B")
-
-        for i in range(sleeps_per_state[state['sm_state']]):
-            if i > sleeps_per_state[state['sm_state']]:
-                break
-            time.sleep(1)
-
-
-# MQTT Client Setup
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
-
-# Set username and password for authentication
-client.username_pw_set(username, password)
-
-# Configure SSL/TLS context (only CA certificate)
-client.tls_set(ca_certs=ca_certs_path)
-
-# Connect to MQTT Broker
-client.connect(broker_address, broker_port)
-
-publisher = threading.Thread(target=publish_data, args=(sm.state, sleeps_per_state, client))
-publisher.start()
-
-# Start the MQTT client's network loop in a separate thread
-client.loop_start()
+mqtt_contrl = MQTTController(broker_address,
+                             broker_port,
+                             username,
+                             password,
+                             ca_certs_path,
+                             sm)
+mqtt_contrl.start_publisher()
 
 while True:
     pprint(sm.state)
